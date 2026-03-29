387bea3d612bff83729c09074183575e73970f2a
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2026-03-29 13:21:58 -0600

    feat(providers): add StoreProvider for zustand context
    - Create StoreProvider component using zustand-utils createContext
    - Wrap app store in React context for component access
    - Support proper TypeScript typing with PropsWithChildren

---
+++ src/core/providers/StoreProvider.tsx
@@ -0,0 +1,13 @@
+import type { PropsWithChildren } from 'react'
+import { createContext } from 'zustand-utils'
+import { useAppStore } from '../stores/app.store'
+
+const { Provider } = createContext()
+
+type Props = PropsWithChildren & {};
+
+export const StoreProvider = ({ children }: Props) => (
+  <Provider createStore={useAppStore}>
+    {children}
+  </Provider>
+)
