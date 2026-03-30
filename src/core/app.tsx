5111dc22c77b6a5fe38842aa2455be88e387315f
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2026-03-29 18:46:52 -0600

    feat: add global styles with Material Design tokens
    - Create src/index.css with MD3 color variables and base styles
    - Link stylesheet in index.html and app.tsx

--- src/core/app.tsx
+++ src/core/app.tsx
@@ -2,6 +2,7 @@ import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { routeTree } from '../routeTree.gen'
+import '../index.css'
 
 const router = createRouter({ routeTree })
 
