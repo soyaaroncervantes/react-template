43f284d162a7e162b35c9b2709e928a2ac3a5c85
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2026-03-31 17:26:47 -0600

    style(theme): wrap ThemeBaseProps type declaration

--- src/features/theme/theme.types.ts
+++ src/features/theme/theme.types.ts
@@ -1,4 +1,5 @@
 import type { ReactWebComponent } from '@lit/react'
 import type { ComponentProps, HTMLAttributes } from 'react'
 
-export type ThemeBaseProps<T extends HTMLElement> = ComponentProps<ReactWebComponent<T>> & HTMLAttributes<T>
+export type ThemeBaseProps<T extends HTMLElement> = ComponentProps<ReactWebComponent<T>> &
+  HTMLAttributes<T>
