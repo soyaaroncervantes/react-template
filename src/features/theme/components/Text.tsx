602d0123681fc5cbb6f55d1642988f8f213177a2
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2026-03-31 17:26:43 -0600

    style(theme): format ThemeText type imports and props

--- src/features/theme/components/Text.tsx
+++ src/features/theme/components/Text.tsx
@@ -1,14 +1,20 @@
-import type { HeadingLevel, HeadingSize, HeadingVariant, M3eHeadingElement } from '@m3e/react/heading'
-import type { PropsWithChildren } from 'react'
+import type {
+  HeadingLevel,
+  HeadingSize,
+  HeadingVariant,
+  M3eHeadingElement,
+} from '@m3e/react/heading'
 import { M3eHeading } from '@m3e/react/heading'
+import type { PropsWithChildren } from 'react'
 import type { ThemeBaseProps } from '../theme.types'
 
-export type TextProps = PropsWithChildren & ThemeBaseProps<M3eHeadingElement> & {
-  level?: HeadingLevel
-  size?: HeadingSize
-  variant?: HeadingVariant
-}
- 
+export type TextProps = PropsWithChildren &
+  ThemeBaseProps<M3eHeadingElement> & {
+    level?: HeadingLevel
+    size?: HeadingSize
+    variant?: HeadingVariant
+  }
+
 export const ThemeText = ({ children, ...props }: TextProps) => {
   return <M3eHeading {...props}>{children}</M3eHeading>
 }
