281511867eee2c82458d00884d6c010d07845097
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2026-04-06 22:15:47 -0600

    feat(store): integrate apiClientsSlice into app store

--- src/core/stores/app.store.ts
+++ src/core/stores/app.store.ts
@@ -1,10 +1,11 @@
 import { create, type StoreApi } from 'zustand'
 import { withSlices } from 'zustand-slices'
 import { createContext } from 'zustand-utils'
+import { apiClientsSlice } from '@/features/clients/api.stores'
 import { themeSlice } from '@/features/theme/theme.stores'
 import { createDevtools } from './devtools.store'
 
-const storeConfig = withSlices(themeSlice)
+const storeConfig = withSlices(themeSlice, apiClientsSlice)
 export type AppStore = ReturnType<typeof storeConfig>
 
 const devtools = createDevtools('App')
