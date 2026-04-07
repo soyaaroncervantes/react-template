7a43ab9ecd461195c4d949dd228772fb2e472885
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2026-04-06 23:40:57 -0600

    fix: update imports for app store

--- src/core/stores/app.store.ts
+++ src/core/stores/app.store.ts
@@ -1,7 +1,7 @@
 import { create, type StoreApi } from 'zustand'
 import { withSlices } from 'zustand-slices'
 import { createContext } from 'zustand-utils'
-import { apiClientsSlice } from '@/features/clients/api.stores'
+import { apiClientsSlice } from '@/core/clients/api.stores'
 import { themeSlice } from '@/features/theme/theme.stores'
 import { createDevtools } from './devtools.store'
 
