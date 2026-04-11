b11f9b35e2be81dcbeea74ce16f99edcedbb9c02
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2026-04-10 21:58:08 -0600

    refactor(store): remove useState from StoreProvider and call getPreferredColorScheme directly

--- src/core/providers/StoreProvider.tsx
+++ src/core/providers/StoreProvider.tsx
@@ -1,10 +1,10 @@
-import { type PropsWithChildren, useState } from 'react'
+import type { PropsWithChildren } from 'react'
 import { createStore, Provider } from '@/core/stores/app.store'
 import { getPreferredColorScheme } from '@/features/theme/usePreferredColorScheme'
 
 type Props = PropsWithChildren & {}
 
 export const StoreProvider = ({ children }: Props) => {
-  const [theme] = useState(getPreferredColorScheme())
+  const theme = getPreferredColorScheme()
   return <Provider createStore={() => createStore({ theme })}>{children}</Provider>
 }
